2c1857eb4df3c1c5e6aed2b768ce30151f290b14
mahmoudmatter12 <Mahmoud.Aziz-El-Din.3230080@CIT.hnu.edu.eg>
2025-02-28 18:03:55 +0200

    modify: App.js in routes

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { PORT } from './config/env.js';
+import authRouter from './routes/auth.routes.js';
 
 const app = express();
 
@@ -10,6 +11,16 @@ app.get('/', (req, res) => {
 app.listen(3000, () => {
     console.log(`Subscribtion Tracker API is running on http://localhost:${PORT}`);
 });
+
+app.use('/auth', authRouter);
+
+// to test that the app is running correctly, run the following command in the terminal:
+// $ curl http://localhost:3000
+// The response should be: Hello World!
+// To test the auth routes, run the following command in the terminal:
+// $ curl http://localhost:3000/auth/sign-up
+// The response should be: {"title":"Register route"}
+
 
 export default app;
 
